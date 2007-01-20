2a100462487ed62bef713c5fd10c88ceb6311230
Neil Roberts <bpeeluk@yahoo.co.uk>
2007-01-20 00:24:15 +0000

    Added code for the timer under Windows

--- timer.c
+++ timer.c
@@ -1,26 +1,42 @@
 #include "config.h"
 
 #include <stdlib.h>
+#ifdef WIN32
+#include <windows.h>
+#else
 #include <sys/time.h>
+#endif
 
 #include "eek.h"
 #include "timer.h"
 
-struct timeval timer_start;
+#ifdef WIN32
+static DWORD timer_start;
+#else
+static struct timeval timer_start;
+#endif
 
 void
 timer_init (void)
 {
+#ifdef WIN32
+  timer_start = GetTickCount ();
+#else
   gettimeofday (&timer_start, NULL);
+#endif
 }
 
 unsigned int
 timer_ticks (void)
 {
+#ifdef WIN32
+  return GetTickCount () - timer_start;
+#else
   struct timeval now;
 
   gettimeofday (&now, NULL);
 
   return (now.tv_sec - timer_start.tv_sec) * 1000
     + (now.tv_usec - timer_start.tv_usec) / 1000;
+#endif
 }
