6b2832cc3c821180b7b0d49ca2106487e3b3bdf7
Neil Roberts <bpeeluk@yahoo.co.uk>
2007-02-19 20:54:21 +0000

    Converted the functions to operate on a Video struct instead of using a global variable. It now no longer uses SDL and instead expects something higher up to draw the display.

--- src/video.h
+++ src/video.h
@@ -3,14 +3,29 @@
 
 #include "stypes.h"
 
-int video_init (UBYTE *memory, int flags);
-void video_update (void);
-void video_quit (void);
-void video_draw_scanline (int line);
-void video_set_start_address (UWORD start);
-void video_set_mode (UBYTE mode);
-void video_set_color (UBYTE logical, UBYTE physical);
+#define VIDEO_YSCALE       2
+#define VIDEO_WIDTH        640
+#define VIDEO_SCREEN_PITCH VIDEO_WIDTH
+#define VIDEO_HEIGHT       (256 * VIDEO_YSCALE)
+#define VIDEO_MEMORY_SIZE  (VIDEO_SCREEN_PITCH * VIDEO_HEIGHT)
 
-#define VIDEO_FULLSCREEN 1
+#define VIDEO_LOGICAL_COLOR_COUNT 16
+
+typedef struct _Video Video;
+
+struct _Video
+{
+  const UBYTE *memory;
+  UBYTE screen_memory[VIDEO_MEMORY_SIZE];
+  UWORD start_address;
+  UBYTE logical_colors[VIDEO_LOGICAL_COLOR_COUNT];
+  UBYTE mode;
+};
+
+void video_init (Video *video, const UBYTE *memory);
+void video_draw_scanline (Video *video, int line);
+void video_set_start_address (Video *video, UWORD start);
+void video_set_mode (Video *video, UBYTE mode);
+void video_set_color (Video *video, UBYTE logical, UBYTE physical);
 
 #endif /* _VIDEO_H */
