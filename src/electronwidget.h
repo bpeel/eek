eb1238819dbf5e1db564dce133bb3cbef45dbec1
Neil Roberts <bpeeluk@yahoo.co.uk>
2007-03-24 10:54:34 +0000

    The electron is now separated from the widget and is wrapped inside an ElectronManager object. This means that multiple widgets could display the same widget. The timing code has been moved into the ElectronManager and the widget reponds to a frame signal instead of directly running the electron.

--- src/electronwidget.h
+++ src/electronwidget.h
@@ -2,14 +2,19 @@
 #define _ELECTRON_WIDGET_H
 
 #include <gtk/gtkwidget.h>
-#include "electron.h"
+#include "electronmanager.h"
 
 #define TYPE_ELECTRON_WIDGET (electron_widget_get_type ())
-#define ELECTRON_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), TYPE_ELECTRON_WIDGET, ElectronWidget))
-#define ELECTRON_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), TYPE_ELECTRON_WIDGET, ElectronWidgetClass))
-#define IS_ELECTRON_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TYPE_ELECTRON_WIDGET))
-#define IS_ELECTRON_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), TYPE_ELECTRON_WIDGET))
-#define ELECTRON_WIDGET_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), TYPE_ELECTRON_WIDGET, ElectronWidgetClass))
+#define ELECTRON_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+                              TYPE_ELECTRON_WIDGET, ElectronWidget))
+#define ELECTRON_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), \
+                                      TYPE_ELECTRON_WIDGET, ElectronWidgetClass))
+#define IS_ELECTRON_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+                                 TYPE_ELECTRON_WIDGET))
+#define IS_ELECTRON_WIDGET_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), \
+                                         TYPE_ELECTRON_WIDGET))
+#define ELECTRON_WIDGET_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+                                        TYPE_ELECTRON_WIDGET, ElectronWidgetClass))
 
 typedef struct _ElectronWidget ElectronWidget;
 typedef struct _ElectronWidgetClass ElectronWidgetClass;
@@ -18,11 +23,12 @@ struct _ElectronWidget
 {
   GtkWidget parent_object;
 
-  Electron *electron;
-  guint timeout;
+  ElectronManager *electron;
 
   unsigned char shift_state, control_state, alt_state;
   int key_override;
+
+  int frame_end_handler;
 };
 
 struct _ElectronWidgetClass
@@ -32,5 +38,7 @@ struct _ElectronWidgetClass
 
 GType electron_widget_get_type ();
 GtkWidget *electron_widget_new ();
+GtkWidget *electron_widget_new_with_electron (ElectronManager *electron);
+void electron_widget_set_electron (ElectronWidget *ewidget, ElectronManager *electron);
 
 #endif /* _ELECTRON_WIDGET_H */
