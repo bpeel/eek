2b4a34fdb2b9fafb97ee20fa90c7ff8665199fa2
Neil Roberts <bpeeluk@yahoo.co.uk>
2007-03-24 10:53:56 +0000

    - The electron is now created outside of the widget using an   ElectronManager. This is then passed to the widget. - The timer module is now longer used

--- src/main.c
+++ src/main.c
@@ -8,9 +8,9 @@
 
 #include "eek.h"
 #include "electronwidget.h"
+#include "electronmanager.h"
 #include "cpu.h"
 #include "electron.h"
-#include "timer.h"
 #include "util.h"
 
 static void
@@ -24,22 +24,22 @@ main (int argc, char **argv)
 {
   char *os_rom = "roms/os.rom";
   GtkWidget *window, *ewidget;
+  ElectronManager *eman;
 
   shortname = util_shortname (argv[0]);
 
   if (argc >= 2)
     os_rom = argv[1];
 
-  /* Initialise the timer */
-  timer_init ();
-
   /* Initialise GTK */
   gtk_init (&argc, &argv);
+  /* Create the electron */
+  eman = electron_manager_new ();
   /* Create the main window */
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   g_signal_connect (G_OBJECT (window), "destroy", G_CALLBACK (main_window_on_destroy), NULL);
   /* Add an electron widget to it */
-  ewidget = electron_widget_new ();
+  ewidget = electron_widget_new_with_electron (eman);
   gtk_container_add (GTK_CONTAINER (window), ewidget);
   gtk_widget_show (ewidget);
 
@@ -48,10 +48,10 @@ main (int argc, char **argv)
     FILE *file;
 
     if ((file = fopen (os_rom, "rb")) == NULL
-	|| electron_load_os_rom (ELECTRON_WIDGET (ewidget)->electron, file) == -1
+	|| electron_load_os_rom (eman->data, file) == -1
 	|| fclose (file) == EOF
 	|| (file = fopen ("roms/basic.rom", "rb")) == NULL
-	|| electron_load_paged_rom (ELECTRON_WIDGET (ewidget)->electron, ELECTRON_BASIC_PAGE, file)
+	|| electron_load_paged_rom (eman->data, ELECTRON_BASIC_PAGE, file)
 	|| fclose (file) == EOF)
     {
       eprintf ("couldn't load rom: %s\n", strerror (errno));
@@ -59,7 +59,9 @@ main (int argc, char **argv)
     }
   }
 
-  cpu_restart (&ELECTRON_WIDGET (ewidget)->electron->cpu);
+  cpu_restart (&eman->data->cpu);
+
+  g_object_unref (eman);
 
   gtk_widget_show (window);
   gtk_main ();
