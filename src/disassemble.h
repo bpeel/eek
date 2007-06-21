a92b18fe6d58b5715d970e99326046b210027153
Neil Roberts <bpeeluk@yahoo.co.uk>
2007-06-21 20:20:13 +0000

    Added macros for the maximum length of data that will be returned from disassemble_instruction.

--- src/disassemble.h
+++ src/disassemble.h
@@ -3,6 +3,10 @@
 
 #include "stypes.h"
 
+#define DISASSEMBLE_MAX_BYTES    3
+#define DISASSEMBLE_MAX_MNEMONIC 3
+#define DISASSEMBLE_MAX_OPERANDS 7
+
 int disassemble_instruction (UWORD address, const UBYTE *bytes,
 			     char *mnemonic, char *operands);
 
